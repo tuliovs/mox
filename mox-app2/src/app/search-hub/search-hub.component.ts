6daecfb7e8ffabc159ba90a363553078fc1bfc50
Tulio <tuliovs@gmail.com>
2018-08-14 01:13:44 -0300

    --END OF VUNERABILITYS jace bug fix, 404 fix

--- mox-app2/src/app/search-hub/search-hub.component.ts
+++ mox-app2/src/app/search-hub/search-hub.component.ts
@@ -1,27 +1,31 @@
 import { ActionStateService } from '@application/_services/action-state/action-state.service';
 import { ScryfallSearchService } from '@application/_services/scryfall-services/search/scryfall-search.service';
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component } from '@angular/core';
 import { List } from '@application/_models/_scryfall-models/models';
+import { tap, catchError } from '../../../node_modules/rxjs/operators';
+import { throwError } from '../../../node_modules/rxjs';
 
 @Component({
   selector: 'app-mox-search-hub',
   templateUrl: './search-hub.component.html',
   styleUrls: ['./search-hub.component.sass']
 })
-export class SearchHubComponent implements OnInit {
+export class SearchHubComponent {
   public param: string;
   public searchResult: List = new List();
+  public animationState: string;
   public resp_time = 0;
   public settings_stats = false;
-  public showLoader = false;
+  public showError = false;
   public searchState = 'closed';
   public selectedCard;
   constructor(
     private _searchService: ScryfallSearchService,
     private _state: ActionStateService
-  ) { }
-
-  ngOnInit() {
+  ) {
+    this._state.getState().subscribe(stt => {
+      this.animationState = stt;
+    });
   }
 
   selectCard(card: any) {
@@ -32,21 +36,34 @@ export class SearchHubComponent implements OnInit {
     }
   }
 
+  isState(p): boolean {
+    return (this.animationState === p);
+  }
+
   searchGo() {
     // console.log('param: ', this.param);
     this._state.setState('loading');
-    this.showLoader = true;
     this.settings_stats = true;
     this.resp_time = Date.now();
-    this._searchService.search(this.param).subscribe(
-        list => {
+    this._searchService.search(this.param).pipe(
+        tap((list) => {
           // this._card = new CardMapper().map(card);
           this.searchResult = list;
           this.resp_time = Date.now() - this.resp_time;
-          this.showLoader = false;
+          this.showError = false;
           this._state.setState('nav');
           console.log('>> ', this.searchResult);
-        }
-      );
+        }),
+        catchError((err) => {
+          console.log(err);
+          if (err.status === 404) {
+            this.showError = true;
+            this.searchResult.data = null;
+            this.settings_stats = false;
+            this._state.setState('error');
+          }
+          return throwError(err);
+        })
+      ).subscribe();
   }
 }
